1da205270e8bd2d5bbe00faea75cb28ad7a965ba
anelisynagano <anelisynagano@gmail.com>
2021-05-06 10:06:14 +0100

    Create/Update/Delete

--- index.js
+++ index.js
@@ -57,6 +57,87 @@ app.get("/products", (req, res) => {
   });
 });
 
+app.get("/products/:id", (req, res) => {
+  const { id } = req.params;
+  connection.query(
+    "SELECT * FROM products WHERE id=?",
+    [id],
+    (err, results) => {
+      if (err) {
+        res.status(500).send("Error retrieving product");
+      } else {
+        res.json(results[0]);
+      }
+    }
+  );
+});
+
+app.get("/products", (req, res) => {
+  const { max_price } = req.query;
+  connection.query(
+    "SELECT * FROM products WHERE price <= ?",
+    [max_price],
+    (err, results) => {
+      if (err) {
+        res.status(500).send("Error retrieving products");
+      } else {
+        res.json(results);
+      }
+    }
+  );
+});
+
+app.post("/products", (req, res) => {
+  const { name, price } = req.body;
+  connection.query(
+    "INSERT INTO products (name, price) VALUES (?, ?)",
+    [name, price],
+    (err, results) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error adding product to db.");
+      } else {
+        const addedProduct = { id: results.insertId, name, price };
+        res.json(addedProduct);
+      }
+    }
+  );
+});
+
+app.put("/products/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, price } = req.body;
+  connection.query(
+    "UPDATE products SET name = ?, price = ? WHERE id = ?",
+    [name, price, id],
+    (err, results) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error updating product to db.");
+      } else {
+        const editedProduct = { id, name, price };
+        res.json(editedProduct);
+      }
+    }
+  );
+});
+
+app.delete("/products/:id", (req, res) => {
+  const { id } = req.params;
+  connection.query(
+    "DELETE FROM products WHERE id = ?",
+    [id],
+    (err, results) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error deleting product from db.");
+      } else {
+        res.send("Product deleted :(");
+      }
+    }
+  );
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
